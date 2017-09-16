e85c9cb3ca86f25aa313214f6f0f23db8d0b3b7e
Daniel Zelaya <wupaz9@gmail.com>
2017-09-15 20:12:40 -0600

    Setup for month rendering

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,13 +4,36 @@ import moment from 'moment';
 import styles from './Calendar.css';
 
 import CalendarHeader from '../CalendarHeader';
+import Month from '../Month';
 
-const Calendar = ({ startDate = moment(), dayCount = 0, countryCode = "US" }) => {
+function renderMonths(startDate, dayCount, countryCode) {
+  const endDate = startDate.clone().add(dayCount, 'days');
+  let currDate = startDate.clone();
+  let months = [];
+
+  while (currDate.month() != endDate.month() || currDate.year() != endDate.year()) {
+      months = [ ...months, <Month startDate={currDate} /> ];
+      currDate.date(1);
+      currDate.add(1, 'month');
+  }
+
+  months = [ ...months, <Month startDate={currDate} dayCount={endDate.date() - currDate.date()} /> ];
+
+  return months;
+}
+
+const Calendar = ({ startDate = "", dayCount = 0, countryCode = "US" }) => {
+  startDate = moment(startDate, "MM/DD/YYYY");
+
+  if (!startDate.isValid() || dayCount <= 0 || countryCode.length <= 0) return null;
+
   return (
     <div className={styles.root}>
       <CalendarHeader />
+      {renderMonths(startDate, dayCount, countryCode)}
     </div>
   );
+
 };
 
 export default Calendar;
