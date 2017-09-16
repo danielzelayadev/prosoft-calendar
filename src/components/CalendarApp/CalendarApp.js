a000e15484ac2e69562f9d2df8bfca082c83bcb2
Daniel Zelaya <wupaz9@gmail.com>
2017-09-15 19:35:21 -0600

    Styles for form and bind inputs to app state

--- src/components/CalendarApp/CalendarApp.js
+++ src/components/CalendarApp/CalendarApp.js
@@ -3,23 +3,44 @@ import React, { Component } from "react";
 import styles from './CalendarApp.css';
 
 class CalendarApp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      startDate: "",
+      dayCount: 1,
+      countryCode: ""
+    };
+  }
+  onStartDateChange(e) {
+    this.setState({ ...this.state, startDate: e.target.value });
+  }
+  onCountryCodeChange(e) {
+    this.setState({ ...this.state, countryCode: e.target.value });
+  }
+  onDayCountChange(e) {
+    this.setState({ ...this.state, dayCount: e.target.value });
+  }
   render() {
+    const { startDate, dayCount, countryCode } = this.state;
+    const { onStartDateChange, onDayCountChange, 
+            onCountryCodeChange } = this;
+
     return (
       <div className={styles.root}>
         <div className={styles.form}>
           <div className={styles['form-group']}>
             <label>Start Date</label>
-            <input type="text" placeholder="08/15/08"/>
+            <input type="text" value={startDate} onChange={onStartDateChange.bind(this)} placeholder="08/15/08"/>
           </div>
           <div className={styles['form-group']}>
             <label>Day Count</label>
-            <input type="number" min="1" placeholder="1" />
+            <input type="number" min="1" value={dayCount} onChange={onDayCountChange.bind(this)} placeholder="1" />
           </div>
           <div className={styles['form-group']}>
             <label>Country Code</label>
-            <input type="text" placeholder="US"/>
+            <input type="text" placeholder="US" value={countryCode} onChange={onCountryCodeChange.bind(this)}/>
           </div>
-          <button>Create Calendar</button>
         </div>
         <div className={styles["calendar-area"]}>Calendar Area</div>
       </div>
