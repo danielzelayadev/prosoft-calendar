29c2aa7d3abf0a377854269b88a54322fcaed6d7
Daniel Zelaya <wupaz9@gmail.com>
2017-09-15 20:27:04 -0600

    Week rendering setup

--- src/components/Month/Month.js
+++ src/components/Month/Month.js
@@ -2,6 +2,7 @@ import React from "react";
 import moment from 'moment';
 
 import MonthHeader from '../MonthHeader';
+import Week from '../Week';
 
 import { getLastDateOfMonth } from '../../helpers';
 
@@ -20,12 +21,34 @@ const months = [
   "December"
 ];
 
+function renderWeeks(startDate, dayCount) {
+  const startDay = startDate.day();
+  const endDate = startDate.clone().add(dayCount, "days");
+  const endDay = endDate.day();
+  const endWeek = Math.floor((dayCount+startDay) / 7);
+
+  let currStartDay = startDay;
+  let currWeek = 0;
+  let weeks = [];
+
+  while (currWeek != endWeek) {
+      weeks = [ ...weeks, <Week key={currWeek} startDay={currStartDay} /> ];
+      currStartDay = 0;
+      currWeek++;
+  }
+
+  weeks = [ ...weeks, <Week key={currWeek} startDay={currStartDay} dayCount={endDay-currStartDay} /> ];
+
+  return weeks;
+}
+
 const Month = ({ startDate, dayCount = getLastDateOfMonth(startDate) - startDate.date() + 1}) => {
   if (dayCount <= 0) return null;
 
   return (
     <div>
       <MonthHeader month={months[startDate.month()]} year={startDate.year()} />
+      {renderWeeks(startDate, dayCount)}
     </div>
   );
 };
