30d00a3b84938ff3b376e58190b3fb1f18aa78ea
Daniel Zelaya <wupaz9@gmail.com>
2017-09-15 20:27:12 -0600

    Add Week component

---
+++ src/components/Week/Week.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Week = ({ startDay = 0, dayCount = 7 }) => {
+  if (dayCount <= 0) return null;
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Week;
